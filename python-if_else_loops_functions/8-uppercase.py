ab9ef5fcf2d502fda0b369ffb02d3d31c40d1104
Mahammadsaleh <mehemmedsalehabbas@gmail.com>
2024-01-27 22:11:45 +0400

    added

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -4,8 +4,6 @@ def uppercase(str):
     for i in str:
         if 97 <= ord(i) <= 122:
             res += chr(ord(i) - 32)
-        elif 65 <= ord(i) <= 90:
-            res += i
-        elif i.isdigit() or i.isspace():
+        else:
             res += i
     print("{}".format(res))
