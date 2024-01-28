e3cee25ea9e532cafb8ede54d5876465fa1fb314
Mahammadsaleh <mehemmedsalehabbas@gmail.com>
2024-01-28 17:13:46 +0400

    fied

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -7,6 +7,6 @@ if __name__ == "__main__":
     elif len(sys.argv) > 2:
         print("{} arguments:".format(len(sys.argv) - 1))
         for i in range(1, len(sys.argv)):
-            print("{} argument.".format(len(sys.argv) - 1))
+            print("{}: {}".format(i, sys.argv[i]))
     elif len(sys.argv) == 1:
-        print("{} argument.".format(len(sys.argv) - 1))
+        print("{} arguments.".format(len(sys.argv) - 1))
