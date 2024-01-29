3a4aedb452f133362b6ae83f33b105831b0fee07
Mahammadsaleh <mehemmedsalehabbas@gmail.com>
2024-01-29 18:55:43 +0400

    added

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -2,7 +2,7 @@
 def search_replace(my_list, search, replace):
     list_ = list()
     for i in range(len(my_list)):
-        if(my_list[i] == search):
+        if (my_list[i] == search):
             list_.insert(i, replace)
         else:
             list_.insert(i, my_list[i])
