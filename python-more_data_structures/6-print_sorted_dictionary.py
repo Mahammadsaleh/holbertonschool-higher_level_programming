c468eac079223eddcb0c5dbdc1dd2aa8479e6e73
Mahammadsaleh <mehemmedsalehabbas@gmail.com>
2024-01-30 14:23:03 +0400

    added

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
+    if not isinstance(a_dictionary, dict):
+        return
     sorted_k = list(a_dictionary.keys())
     sorted_k.sort()
     sorted_dict = {i: a_dictionary[i] for i in sorted_k}
