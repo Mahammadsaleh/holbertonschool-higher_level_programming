a1f646fc4a53ac836c37af589626a9fb798f9bfb
Mahammadsaleh <mehemmedsalehabbas@gmail.com>
2024-02-02 04:32:57 +0400

    fixed

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -5,7 +5,7 @@ def roman_to_int(roman_string):
                  "D": 500, "M": 1000}
     sum = 0
     last_digit_val = 0
-    if roman_string is None and roman_string.isalpha():
+    if roman_string is None or not isinstance(roman_string, str):
         return sum
     for roman in roman_string[::-1]:
         cur_val = value_map[roman]
