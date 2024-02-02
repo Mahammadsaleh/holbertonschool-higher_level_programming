d1965c6e6597db3fd9cb07bb439574246f17156c
Mahammadsaleh <mehemmedsalehabbas@gmail.com>
2024-02-02 04:26:30 +0400

    hot fix

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -3,7 +3,8 @@ def roman_to_int(roman_string):
     value_map = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
     sum = 0
     last_digit_val = 0
-
+    if roman_string == None and roman_string.isalpha():
+        return
     for roman in roman_string[::-1]:
         cur_val = value_map[roman]
 
