a8a9bc10e27354624db8c9278dac4bc654a3555f
Mahammadsaleh <mehemmedsalehabbas@gmail.com>
2024-02-04 05:58:56 +0400

    added

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -48,7 +48,11 @@ class Square:
         if self.__size == 0:
             print()
         else:
-            for _ in range(self.__position[1]):
+            for i in range(self.position[1]):
                 print()
-            for _ in range(self.__size):
-                print(" " * self.__position[0] + "#" * self.__size)
+            for j in range(0, self.size):
+                for e in range(self.position[0]):
+                    print(" ", end="")
+                for j in range(self.size):
+                    print("#", end="")
+                print()
