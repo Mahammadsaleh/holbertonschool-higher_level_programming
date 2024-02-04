cf68c947cce71ed25f7d94200671f44ac2a6a5d0
Mahammadsaleh <mehemmedsalehabbas@gmail.com>
2024-02-04 22:44:45 +0400

    added

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""documentaion"""
+
+
+def text_indentation(text):
+    """Text indentation"""
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+    full = ""
+    counter = 0
+    for i in range(len(text)):
+        full += text[i]
+        if text[i] == '.' or text[i] == '?' or text[i] == ':':
+            if not counter == 0:
+                print("{}".format(full[1:]), end='')
+            else:
+                print("{}".format(full), end='')
+            print()
+            print()
+            full = ""
+            counter += 1
