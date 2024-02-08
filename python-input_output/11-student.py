af4566300a291094d5997c37e36fd5954a5cbec0
Mahammadsaleh <mehemmedsalehabbas@gmail.com>
2024-02-09 03:47:20 +0400

    fixed

--- python-input_output/11-student.py
+++ python-input_output/11-student.py
@@ -18,6 +18,7 @@ class Student:
             return chosen
         else:
             return self.__dict__
+
     def reload_from_json(self, json):
         for key, value in json.items():
             setattr(self, key, value)
