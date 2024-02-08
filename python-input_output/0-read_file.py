6b41544407353a21eb552e65e8b08a9cb4c25367
Mahammadsaleh <mehemmedsalehabbas@gmail.com>
2024-02-08 19:58:16 +0400

    0-read_file.py added

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -6,4 +6,5 @@ def read_file(filename=""):
     """Read the File"""
     with open(filename, mode="r", encoding="utf-8") as my_file:
         text = my_file.read()
-        print(text)
+        print(text, end="")
+        my_file.close()
