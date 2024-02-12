5e0fa106c894062db6d481e0ace3b1cdc66d3a8f
Mahammadsaleh <mehemmedsalehabbas@gmail.com>
2024-02-12 16:43:48 +0400

    Validate attributes

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -32,7 +32,7 @@ class Rectangle(Base):
     @height.setter
     def height(self, value):
         if not isinstance(value, int):
-            raise TypeError("heigth must be an integer")
+            raise TypeError("height must be an integer")
         if value <= 0:
             raise ValueError("height must be > 0")
         self.__height = value
