df7e854881c5e3aecc8a93c656b4eb4440839164
Mahammadsaleh <mehemmedsalehabbas@gmail.com>
2024-02-12 20:08:30 +0400

    added

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -10,6 +10,15 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         super().__init__(size, size, x, y)
 
+    @property
+    def size(self):
+        return self.width  # Since width = height for a Square, we can return either
+
+    @size.setter
+    def size(self, value):
+        # Since a Square's width and height are always equal, setting size should update both
+        self.width = value
+        self.height = value
     def __str__(self):
         return "[Square] ({}) {}/{} - {}".format(self.id,
                                                  self.x,
