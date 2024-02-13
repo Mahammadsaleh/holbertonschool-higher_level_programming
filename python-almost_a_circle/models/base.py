69ac6996330bc0d5587105c5489aad6c2ddcfedd
Mahammadsaleh <mehemmedsalehabbas@gmail.com>
2024-02-13 20:21:47 +0400

    fix

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -19,7 +19,8 @@ class Base:
     @staticmethod
     def to_json_string(list_dictionaries):
         """Documentation of to_json_string"""
-        if list_dictionaries is None or not list_dictionaries:
-            return []
+
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
         else:
-            return json.dumps(list_dictionaries)
+            return (json.dumps(list_dictionaries))
