51f238d30e1e75aec46ef7aaeadf733112d5a7da
Mahammadsaleh <mehemmedsalehabbas@gmail.com>
2024-02-14 18:23:24 +0400

    Dictionary to Instance

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -48,6 +48,9 @@ class Base:
     @classmethod
     def create(cls, **dictionary):
 
-        dummy = cls(1, 1)
+        if cls.__name__ == "Rectangle":
+            dummy = cls(1, 1)
+        else:
+            dummy = cls(1)
         dummy.update(**dictionary)
         return dummy
