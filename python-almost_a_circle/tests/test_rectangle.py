ce3c23e247426bb8d58fc2c447d35eea61ed5ab9
Mahammadsaleh <mehemmedsalehabbas@gmail.com>
2024-02-15 20:55:04 +0400

    some fixation

--- python-almost_a_circle/tests/test_rectangle.py
+++ python-almost_a_circle/tests/test_rectangle.py
@@ -114,7 +114,7 @@ class TestRectangle(unittest.TestCase):
         with open("Rectangle.json", "r") as file:
             self.assertEqual(file.read(), "[]")
 
-    def test_save_to_file_empty_list(self):
+    def test_save_to_file_empty(self):
         Rectangle.save_to_file([])
         with open("Rectangle.json", mode="r") as file:
             self.assertEqual(file.read(), "[]")
