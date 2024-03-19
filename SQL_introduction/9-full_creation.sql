45b1f698465525d7fb05af62aba9df36a7b8f4eb
Mahammadsaleh <mehemmedsalehabbas@gmail.com>
2024-03-19 16:05:01 +0400

    Full creation

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -12,4 +12,5 @@ INSERT INTO second_table (id, name, score) VALUES
 (1, 'John', 10),
 (2, 'Alex', 3),
 (3, 'Bob', 14),
-(4, 'George', 8);
+(4, 'George', 8)
+ON DUPLICATE KEY UPDATE name=VALUES(name), score=VALUES(score);
