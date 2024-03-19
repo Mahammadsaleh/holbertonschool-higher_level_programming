2fe324ab1243af9eb5bda73c16ad6519c732a85e
Mahammadsaleh <mehemmedsalehabbas@gmail.com>
2024-03-20 01:53:54 +0400

    My privileges!

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,14 +1,3 @@
 -- Connect to MySQL server
-mysql -u your_username -p
-
--- Run the following commands in MySQL prompt
-
--- List privileges for user_0d_1
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
-
--- List privileges for user_0d_2
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
-
--- Exit MySQL prompt
-exit;
-
