a430715bdaa852165780ec7941bb71e0b065fcb4
Mahammadsaleh <mehemmedsalehabbas@gmail.com>
2024-03-23 17:54:11 +0400

    Filter states

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,34 +1,25 @@
 #!/usr/bin/python3
-"Documentation"
+"""Documentation"""
 
 
 import MySQLdb
 import sys
 
-def main():
-    # Unpack command line arguments
-    username, password, database = sys.argv[1], sys.argv[2], sys.argv[3]
-
-    # Connect to the MySQL database
-    db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database)
-
-    # Create a cursor object
-    cursor = db.cursor()
-
-    # Execute the SQL query
-    query = "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC"
-    cursor.execute(query)
-
-    # Fetch all the rows
-    rows = cursor.fetchall()
-
-    # Print each row
-    for row in rows:
-        print(row)
-
-    # Close the cursor and the connection
-    cursor.close()
-    db.close()
 
 if __name__ == "__main__":
-    main()
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    db = MySQLdb.connect(host="localhost",
+                         port=3306, user=username,
+                         passwd=password, db=database)
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states\
+                   WHERE states.name LIKE 'N%'\
+                   ORDER BY id ASC")
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+    cursor.close()
+    db.close()
