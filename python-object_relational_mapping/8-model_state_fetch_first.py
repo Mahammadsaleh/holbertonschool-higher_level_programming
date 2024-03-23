5fbc53cf38e128e413fd7f46d0db4009398111e4
Mahammadsaleh <mehemmedsalehabbas@gmail.com>
2024-03-24 01:45:09 +0400

    First state

---
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+Start link class to table in database
+"""
+
+
+from model_state import Base, State
+from sys import argv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+def main():
+    if len(argv) != 4:
+        return
+    username = argv[1]
+    password = argv[2]
+    database = argv[3]
+
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                           .format(username, password, database),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    for state in session.query(State).order_by(State.id).all():
+        print("{}: {}".format(state.id, state.name))
+        break
+
+    session.close()
+
+
+if __name__ == "__main__":
+    main()
